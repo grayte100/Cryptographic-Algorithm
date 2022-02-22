2b008ca763aaf8c13764598400938e753935bcb0
grayte100 <65009588+grayte100@users.noreply.github.com>
2022-02-22 15:44:13 +0100

    Update complete random integer cryptographic algorithm.py

--- complete random integer cryptographic algorithm.py
+++ complete random integer cryptographic algorithm.py
@@ -1,17 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-#This is an encryption algorithm
+#This is an encryption and decryption algorithm
 #First, a dictionary containing all alphabets and their numerical equivalents is created
 dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g':7, 'h':8, 'i':9, 'j':10,'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17
         , 'r':18, 's':19, 't': 20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26}
 
 
-#Then the password is inputted
+#Then the password or data to be encrypted is inputted
 password = input ("Enter your password: ").lower()
 passcode = []
 for i in password:
